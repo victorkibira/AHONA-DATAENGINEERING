ccb6d109403caddbd4dab143c067f73fefa9ae72
victorkibira <144794667+victorkibira@users.noreply.github.com>
2023-09-22 21:11:01 +0300

    add

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,12 @@
-
 import sys
+
 
 import pandas as pd
 
 print(sys.argv)
 
-
 day = sys.argv[1]
 
-#lets do some fancy stuff with pandas
+#some fancy stuff with pandas
 
-print(f'job completed successfully on day = {day}')
+print(f"job completed successfully on day = {day}")
